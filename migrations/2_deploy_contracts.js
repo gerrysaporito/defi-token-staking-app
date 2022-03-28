d4373108cb84d46faa03c52ac14c1871ae0559dc
Gerry Saporito <gerrysaporito@gmail.com>
2022-03-28 16:38:23 -0400

    Deployed and validated disbursements of tokens to 'investors'

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,28 @@ const Tether = artifacts.require('Tether');
 const RWD = artifacts.require('RWD');
 const DecentralBank = artifacts.require('DecentralBank');
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
+  // Deploy mock Tether contract
   await deployer.deploy(Tether);
+  const tether = await Tether.deployed();
+
+  // Deploy mock reward token contract
   await deployer.deploy(RWD);
-  await deployer.deploy(DecentralBank);
+  const rwd = await RWD.deployed();
+
+  // Deploy Decentral Bank contract
+  await deployer.deploy(DecentralBank, rwd.address, tether.address);
+  const decentralBank = await DecentralBank.deployed();
+
+  // Transfer all RWD tokens to Decentral Bank
+  await rwd.transfer(decentralBank.address, '1000000000000000000000000'); // Taken from rwd contract
+  await tether.transfer(accounts[1], '100000000000000000000'); // Taken from rwd contract
 };
+
+/*
+===== TRUFFLE CONSOLE =====
+tether = await Tether.deployed();
+accounts = await web3.eth.getAccounts();
+balance = await tether.balanceOf(accounts[1]);
+convertBalance = web3.utils.fromWei(balance);
+*/
--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: NONE
 pragma solidity ^0.8.13;
 
+import './RWD.sol';
+import './Tether.sol';
+
 contract DecentralBank {
   address public owner;
   string public name;
+  Tether public tether;
+  RWD public rwd;
 
-  constructor () {
+  constructor (RWD _rwd, Tether _tether) {
     name = 'Decentral Bank';
+    rwd = _rwd;
+    tether = _tether;
   }
 }
