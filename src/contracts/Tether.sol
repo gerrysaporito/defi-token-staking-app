0cd9403663fd84f655baaeb7c3c4e4b22c69d8ee
Gerry Saporito <gerrysaporito@gmail.com>
2022-03-28 15:58:13 -0400

    Created the functions needed for wallets to make transactions with Tether

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -7,23 +7,83 @@ contract Tether {
   uint public totalSupply;
   uint public decimals;
 
+  event Transfer (
+    address indexed _from,
+    address indexed _to,
+    uint _value
+  );
+
+  event Approval (
+    address indexed _owner,
+    address indexed _spender,
+    uint _value
+  );
+
+  mapping(
+    address => uint256
+  ) public balanceOf;
+
+  mapping(
+    address => mapping(
+      address => uint256
+    )
+  ) public allowance;
+
   constructor() {
     name = 'Mock Tether Token';
     symbol = 'mUSDT';
     totalSupply = 1000000000000000000000000;
     decimals = 18;
+    balanceOf[msg.sender] = totalSupply;
   }
 
-  // modifier restricted() {
-  //   if (msg.sender == owner) _;
-  // }
+  /*
+   * Transfer funds from one wallet to another.
+   * Origin wallet must have enough funds to complete the transfer.
+   */
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    // Check if sender has enough funds to transfer amount
+    require(balanceOf[msg.sender] >= _value);
 
-  // function set_completed(uint completed) public restricted {
-  //   last_completed_migration = completed;
-  // }
+    // Transaction between wallets
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
 
-  // function upgrade(address new_address) public restricted {
-  //   Migrations upgraded = Migrations(new_address);
-  //   upgraded.set_completed(last_completed_migration);
-  // }
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /*
+   * Allow 3rd party dapps to make a transaction on a wallet's behalf.
+   * Allows a 3rd party dapp to put a deposit for a wallet.
+   *
+   * msg.sender = 3rd party
+   */
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /*
+   * Allow 3rd party dapps to make a transaction on a wallet's behalf.
+   * Allows a 3rd party dapps to accept a transaction if it's approved for the wallets.
+   * 3rd party dapp must ensure that origin wallet must have enough funds to complete the transfer.
+   *
+   * msg.sender = 3rd party
+   */
+  function transferFrom (address _from, address _to, uint256 _value) public returns (bool success) {
+    // Check if sender has enough funds to transfer amount
+    require(balanceOf[msg.sender] >= _value);
+    require(allowance[msg.sender][_from] >= _value);
+
+    // Transaction between wallets
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+    allowance[msg.sender][_from] -= _value;
+
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
 }
