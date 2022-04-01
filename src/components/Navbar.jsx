a5f92adf981213b47dd7eee96f5d41613f2cf1ab
Gerry Saporito <gerrysaporito@gmail.com>
2022-03-31 21:53:21 -0400

    Created navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import BANK from '../bank.png';
+
+export const Navbar = () => {
+  return (
+    <nav
+      className="navbar navbar-dark fixed-top shadow p-0"
+      style={{ backgroundColor: 'black', height: '50px' }}
+    >
+      <a
+        className="navbar-brand col-sm-3 col-md-2 mr-0"
+        style={{ color: 'white' }}
+      >
+        <img
+          src={BANK}
+          width="50"
+          height="30"
+          className="d-inline-block align-top"
+          alt="Bank image"
+        />
+        &nbsp; DAPP Yield Staking (Decentralied Banking)
+      </a>
+      <ul className="navbar-nav px-3">
+        <li className="text-nowrap d-none nav-item d-sm-none d-sm-block">
+          <small className="" style={{ color: 'white' }}>
+            ACCOUNT NUMBER
+          </small>
+        </li>
+      </ul>
+    </nav>
+  );
+};
